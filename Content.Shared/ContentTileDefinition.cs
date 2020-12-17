1afcfde781c791316ad0f385d3eba4d2dfb85352
20kdc <asdd2808@gmail.com>
2020-12-17 21:40:10 +0000

    [RP8NT] Add some tiles.

---
+++ Content.Shared/ContentTileDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Robust.Shared.Interfaces.Map;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+using YamlDotNet.RepresentationModel;
+
+namespace Content.Shared
+{
+    [UsedImplicitly]
+    [Prototype("tile")]
+    public sealed class ContentTileDefinition : IPrototype, IIndexedPrototype, ITileDefinition
+    {
+        string IIndexedPrototype.ID => Name;
+
+        public string Name { get; private set; }
+        public ushort TileId { get; private set; }
+        public string DisplayName { get; private set; }
+        public string SpriteName => Name;
+        public float Friction => 0;
+
+        public void AssignTileId(ushort id)
+        {
+            TileId = id;
+        }
+
+        public void LoadFrom(YamlMappingNode mapping)
+        {
+            Name = mapping.GetNode("name").ToString();
+            DisplayName = mapping.GetNode("display_name").ToString();
+        }
+
+    }
+}
